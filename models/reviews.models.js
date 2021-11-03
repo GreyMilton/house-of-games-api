44306a98877467d1ee96c31ebf2b29975309992d
Grey Milton's MBP <grey.milton@gmail.com>
2021-11-03 11:58:03 +0000

    completes refactor of get /api/reviews/:review_id

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -2,49 +2,33 @@ const db = require('../db/connection.js');
 
 
 function fetchReview(id) {
-  // I need to rewrite this queryStr so that it:
-  // Selects the full row from review table that matches review_id
-  // Counts the amount of comments in the comments table that refer to it (with the matching review_id)
-  // Appends a new column name onto the review table row: comment_count (with the count of comments relating that review_id)
-  // returns the full row plus appension (is that a word?)
+  // my old two query version:
 
-  // I could do all the above... or I could use js to find the count, and do a second query instead
+  // let queryStr = `SELECT * FROM reviews WHERE review_id = $1`
+  // return db.query(queryStr, [id])
+  // .then((response) => {
+  //   let queryStr2 = `SELECT COUNT(review_id) AS comment_count
+  //   FROM comments WHERE review_id = $1;`
+  //   return Promise.all([response.rows[0], db.query(queryStr2, [id])])
+  // })
+  // .then((response) => {
+  //   const review = response[0];
+  //   review.comment_count = Number(response[1].rows[0].comment_count);
+  //   return review;
+  // })
 
-  let queryStr = `SELECT * FROM reviews WHERE review_id = $1`
+  // my current version using one query:
+  let queryStr = `
+    SELECT reviews.*, COUNT(comment_id) ::INT AS comment_count
+    FROM reviews
+    LEFT JOIN comments ON comments.review_id = reviews.review_id
+    WHERE reviews.review_id = $1
+    GROUP BY reviews.review_id;`;
   return db.query(queryStr, [id])
   .then((response) => {
-    let queryStr2 = `SELECT COUNT(review_id) AS comment_count
-    FROM comments WHERE review_id = $1;`
-    return Promise.all([response.rows[0], db.query(queryStr2, [id])])
+    console.log(response.rows);
+    return response.rows[0];
   })
-  .then((response) => {
-    const review = response[0];
-    review.comment_count = Number(response[1].rows[0].comment_count);
-    return review;
-  })
-
-
-
-
-  // my attempt to acheive the above with only one query:
-
-  // SELECT reviews.*, COUNT(comment_id) AS comment_count
-  // FROM reviews
-  // LEFT JOIN comments ON comments.review_id = reviews.review_id
-  // GROUP BY review_id
-  // WHERE review_id = $1;   <--- I'm not %100 where this line should go
-
-
-  // let queryStr = `
-  //   SELECT reviews.*, COUNT(comment_id) AS comment_count
-  //   FROM reviews
-  //   LEFT JOIN comments ON comments.review_id = reviews.review_id
-  //   GROUP BY reviews.review_id;`;
-  // return db.query(queryStr/*, [id]*/)
-  // .then((response) => {
-  //   console.log(response.rows);
-  //   return response.rows[0];
-  // })
 
 };
 
